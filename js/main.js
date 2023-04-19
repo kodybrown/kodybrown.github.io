d4882028e4c07709b30d0a15b9c7bfb55bf5ae6d
Kody Brown <thewizard@wasatchwizard.com>
2023-04-19 12:17:12 -0600

    removed extra js. added bootstrap.js, jquery, and modernizr.

--- js/main.js
+++ js/main.js
@@ -18,11 +18,3 @@ function addOnLoadEvent(func)
 		}
 	}
 }
-
-$(function () {
-  var includes = $('[data-include]')
-  $.each(includes, function () {
-    var file = 'views/' + $(this).data('include') + '.html'
-    $(this).load(file)
-  })
-})
