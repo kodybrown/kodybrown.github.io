1827faddae0803ea99ca1a3199a660e357156a9e
Kody Brown <thewizard@wasatchwizard.com>
2026-03-10 20:15:02 -0600

    formatted files

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function (cb) {
-  gulp.src('*.scss')
+  gulp
+    .src('*.scss')
     .pipe(sass())
-    .pipe(gulp.dest(function (f) {
-      return f.base;
-    }));
+    .pipe(
+      gulp.dest(function (f) {
+        return f.base;
+      }),
+    );
   cb();
 });
 
-gulp.task('default', gulp.series('sass', function (cb) {
-  gulp.watch('*.scss', gulp.series('sass'));
-  cb();
-}));
+gulp.task(
+  'default',
+  gulp.series('sass', function (cb) {
+    gulp.watch('*.scss', gulp.series('sass'));
+    cb();
+  }),
+);
